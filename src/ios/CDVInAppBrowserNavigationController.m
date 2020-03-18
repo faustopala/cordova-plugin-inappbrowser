b767e5ea3624f1cc1a676c6adaef117491c7b7a9
faustopala <42836071+faustopala@users.noreply.github.com>
2020-03-18 02:29:03 +0100

    Update CDVInAppBrowserNavigationController.m

--- src/ios/CDVInAppBrowserNavigationController.m
+++ src/ios/CDVInAppBrowserNavigationController.m
@@ -33,6 +33,12 @@
 
     CGRect statusBarFrame = [self invertFrameIfNeeded:[UIApplication sharedApplication].statusBarFrame];
     statusBarFrame.size.height = STATUSBAR_HEIGHT;
+ if (@available( iOS 11.0, * )) {
+    if ([[[UIApplication sharedApplication] keyWindow] safeAreaInsets].bottom > 0) {
+        // iPhone with notch
+        statusBarFrame.size.height = 44.0
+    }
+}
     // simplified from: http://stackoverflow.com/a/25669695/219684
 
     UIToolbar* bgToolbar = [[UIToolbar alloc] initWithFrame:statusBarFrame];
