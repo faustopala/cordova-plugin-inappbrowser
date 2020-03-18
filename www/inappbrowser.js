420e275bf0981e5c5eb071c2315b7888a85d1239
faustopala <42836071+faustopala@users.noreply.github.com>
2020-03-18 02:24:04 +0100

    Update inappbrowser.js

--- www/inappbrowser.js
+++ www/inappbrowser.js
@@ -66,9 +66,6 @@
         hide: function (eventname) {
             exec(null, null, 'InAppBrowser', 'hide', []);
         },
-        poba: function (eventname) {
-            exec(null, null, 'InAppBrowser', 'poba', []);
-        },
         addEventListener: function (eventname, f) {
             if (eventname in this.channels) {
                 this.channels[eventname].subscribe(f);
